f161c957db8b0388f53ab62ceee971410922a88e
Keita Furuse <keitafuruse@KeitanoMacBook-Pro.local>
2022-08-27 19:28:32 +0900

    create useTinyState

---
+++ index.ts
@@ -0,0 +1,46 @@
+import { useState, useEffect } from "react";
+import { TinyEmitter } from "tiny-emitter";
+
+const emitter = new TinyEmitter();
+
+const EventBus = {
+  $emit: (unique: symbol) => emitter.emit(unique as any),
+  $on: (unique: symbol, callback: () => void) =>
+    emitter.on(unique as any, callback),
+};
+
+class ProxyMap extends Map<symbol, any> {
+  get(key: symbol) {
+    return super.get(key);
+  }
+  set(key: symbol, value: any) {
+    EventBus.$emit(key);
+    return super.set(key, value);
+  }
+}
+
+const tinyState = new ProxyMap();
+
+export function useTinyState<T>(unique: symbol) {
+  const [state, setState] = useState<T>(tinyState.get(unique));
+
+  useEffect(() => {
+    EventBus.$on(unique, () => {
+      setState(tinyState.get(unique));
+    });
+  }, []);
+
+  return [state];
+}
+
+export function makeState<T>(arg: T) {
+  const unique: unique symbol = Symbol();
+  tinyState.set(unique, arg);
+  return {
+    key: unique,
+    setter: (arg: T) => {
+      tinyState.set(unique, arg);
+      EventBus.$emit(unique);
+    },
+  };
+}
